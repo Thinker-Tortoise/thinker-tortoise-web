0c9dbac752a829934672ffbaeb3f111d5640d50a
HanPauPau <24878629+punlimin@users.noreply.github.com>
2025-10-19 12:26:53 +0800

    update image access

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+const repoName = "thinker-tortoise-web";
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   images: {
     unoptimized: true,
   },
+  assetPrefix: isGithubActions ? `/${repoName}/` : undefined,
+  basePath: isGithubActions ? `/${repoName}` : undefined,
+  trailingSlash: true,
 };
 
 export default nextConfig;
